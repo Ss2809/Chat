4abd0b1c551825154c8c4b14be868cb205e431e0
Sushil <sushilpawar2321@gmail.com>
2026-02-10 18:15:48 +0530

    resent

---
+++ Backend/config/sendEmail.js
@@ -0,0 +1,49 @@
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const sendmail = async (toEmail, subject, htmltemplate) => {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "onboarding@resend.dev", // free plan madhe hech use kara
+      to: toEmail,
+      subject: subject,
+      html: htmltemplate,
+    });
+
+    if (error) {
+      console.error("Email error:", error);
+      return false;
+    }
+
+    console.log("Email sent:", data);
+    return true;
+  } catch (err) {
+    console.error("Send email failed:", err);
+    return false;
+  }
+};
+
+const sendrestpass = async (toEmail, subject, htmltemplate) => {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: toEmail,
+      subject: subject,
+      html: htmltemplate,
+    });
+
+    if (error) {
+      console.error("Reset email error:", error);
+      return false;
+    }
+
+    console.log("Reset email sent:", data);
+    return true;
+  } catch (err) {
+    console.error("Send reset email failed:", err);
+    return false;
+  }
+};
+
+module.exports = { sendmail, sendrestpass };
