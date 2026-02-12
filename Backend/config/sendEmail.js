369319b0f6deb334dd167889f6b2710b7e03488d
Sushil pawar <sushilpawar2321@gmail.com>
2026-02-12 21:01:40 +0530

    Enable logging and debugging for email sending

--- Backend/config/sendEmail.js
+++ Backend/config/sendEmail.js
@@ -9,6 +9,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+   logger: true,
+    debug: true,
 });
 
 const smtpFrom = process.env.SMTP_FROM || process.env.SMTP_USER;
