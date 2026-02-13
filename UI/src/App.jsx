2cfa08984228421b00e56b139a0ed55820e75860
Sushil <sushilpawar2321@gmail.com>
2026-02-13 16:06:21 +0530

    Switch to HashRouter to fix page refresh 404

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
@@ -11,7 +11,7 @@ import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
@@ -23,7 +23,7 @@ function App() {
       <Route path="/profile" element={<Profile />} />
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
