dd5cf0654a5477625d6926d5f54b841cce189659
Sushil <sushilpawar2321@gmail.com>
2026-02-18 16:29:40 +0530

    fasle

--- Backend/config/sendEmail.js
+++ Backend/config/sendEmail.js
@@ -3,9 +3,13 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",   
   port: 587,
+  secure: false,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+   tls: {
+    rejectUnauthorized: false,
   },
 });
 
